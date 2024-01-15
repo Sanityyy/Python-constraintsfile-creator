e8a07c248f51aa572d175e04cc1cb43770d285ca
Stian N <stiannordholm@gmail.com>
2024-01-15 12:39:16 +0100

    Add functionality to enable/disable different components

--- run.py
+++ run.py
@@ -1,18 +1,21 @@
 import PySimpleGUI as sg
 import os
 
+
+clear = lambda: os.system('cls')
+
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Clock singal')],
-            [sg.Checkbox('Enable', default=False)],
+            [sg.Checkbox('Enable', default=False)], #0
             [sg.Text("Switches")],
-            [sg.Checkbox('Enable', default=False)],
+            [sg.Checkbox('Enable', default=False)], #1
             [sg.Text("LEDs")],
-            [sg.Checkbox('Enable', default=False)],
+            [sg.Checkbox('Enable', default=False)], #2
             [sg.Text("7 segment display")],
-            [sg.Checkbox('Enable', default=False)],
+            [sg.Checkbox('Enable', default=False)], #3
             [sg.Text("Buttons")],
-            [sg.Checkbox('Enable', default=False)],
+            [sg.Checkbox('Enable', default=False)], #4
             [sg.Button('Ok'), sg.Button('Cancel')] 
          ]
 
@@ -30,7 +33,19 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[1])
+    
+    clear()
+
+    if values[0]:
+        print("Clock signal enabled")
+    if values[1]:
+        print("Switches enabled")
+    if values[2]:
+        print("LEDs enabled")
+    if values[3]:  
+        print("7 segment display enabled")
+    if values[4]:
+        print("Buttons enabled")
     
 
 window.close()
